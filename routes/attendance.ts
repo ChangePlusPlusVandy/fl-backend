f5b8eee7bb6bcdc5a9a2e5df063ac3fba6ac7f7d
allanwzhang <33790915+allanwzhang@users.noreply.github.com>
2023-11-28 19:31:49 -0500

    attendance boilerplate created (#12)

--- routes/attendance.ts
+++ routes/attendance.ts
@@ -1,5 +1,16 @@
 import express from "express";
-import { getAllAttendance } from "../controllers/attendance-controller";
+import {
+  findAttendance,
+  showAttendance,
+  createAttendance,
+  updateAttendance,
+  deleteAttendance,
+} from "../controllers/attendance-controller";
+
 export const attendanceRouter = express.Router();
 
-attendanceRouter.get("/", getAllAttendance);
+attendanceRouter.get("/", findAttendance);
+attendanceRouter.get("/:attendanceId", showAttendance);
+attendanceRouter.post("/:attendanceId", createAttendance);
+attendanceRouter.put("/:attendanceId", updateAttendance);
+attendanceRouter.delete("/:attendanceId", deleteAttendance);
