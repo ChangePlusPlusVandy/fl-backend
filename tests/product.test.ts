83fa9e6a3f0243118126b45dda69fc7c48fbe891
leozhang6 <47089621+leozhang6@users.noreply.github.com>
2023-11-29 00:59:00 -0600

    jest framework set up (#13)
    Co-authored-by: Gabriel Dong <gabe@Gabriels-MacBook-Pro.local>

---
+++ tests/product.test.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { describe, expect, test } from "@jest/globals";
+import { app } from "..";
+import dotenv from "dotenv";
+import request from "supertest";
+
+/* Connecting to the database before each test. */
+beforeEach(async () => {
+  await mongoose.connect(process.env.MONGO_URI || "");
+});
+
+/* Closing database connection after each test. s*/
+afterEach(async () => {
+  await mongoose.connection.close();
+});
+
+describe("GET /attendance/", () => {
+  it("should return attendance", async () => {
+    const res = await request(app).get("/attendance");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBeGreaterThan(0);
+  });
+});
