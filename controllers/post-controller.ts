5da9374ea8f23221abfbb31a6ac8835b4800fc9a
22rohanr <22rohanr@students.harker.org>
2023-11-28 18:25:28 -0600

    finished

--- controllers/post-controller.ts
+++ controllers/post-controller.ts
@@ -1,5 +1,141 @@
-import express, { Request, Response } from "express";
+import { Request, Response } from "express";
+import { Post } from "../models/post.model";
+import { CommonErrors } from "../utils/common-errors";
 
-export const getAllPosts = async (req: Request, res: Response) => {
-  res.send("getAllPosts works");
-};
+// GET /
+// @TODO: add filtering
+export const findPosts = async (request: Request, response: Response) => {
+  try {
+    const { filters } = request.body;
+
+    const posts = await Post.find(filters ?? {});
+
+    return response
+      .status(200)
+      .json(posts);
+  } catch (e) {
+    return response
+      .status(500)
+      .json({ error: e });
+  }
+}
+
+// GET /{oid}
+export const showPost = async (request: Request, response: Response) => {
+  const { postId } = request.params;
+
+  if (!postId) {
+    return response
+      .status(404)
+      .json({ error: CommonErrors.NotFound });
+  }
+
+  try {
+    const post = await Post.findById(postId);
+
+    return response
+      .status(200)
+      .json(post);
+  } catch (e) {
+    return response
+      .status(500)
+      .json({ error: e });
+  }
+}
+
+// POST /
+export const createPost = async (request: Request, response: Response) => {
+  try {
+    const post = new Post(request.body);
+
+    const validation = post.validateSync();
+
+    if (validation) {
+      return response
+        .status(400)
+        .json({ error: validation.message });
+    }
+
+    await post.save();
+
+    return response
+      .status(200)
+      .json(post);
+  } catch (e) {
+    return response
+      .status(500)
+      .json({ error: e });
+  }
+}
+
+// PUT /{oid}
+export const updatePost = async (request: Request, response: Response) => {
+  const { postId } = request.params;
+
+  if (!postId) {
+    return response
+      .status(400)
+      .json({ error: CommonErrors.BadRequest });
+  }
+
+  try {
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return response
+        .status(404)
+        .json({ error: CommonErrors.NotFound });
+    }
+
+    const replacement = new Post(request.body);
+
+    const validation = replacement.validateSync();
+
+    if (validation) {
+      return response
+        .status(400)
+        .json({ error: validation.message });
+    }
+
+    const result = await Post.updateOne({ _id: postId }, request.body);
+
+    return response
+      .status(204)
+      .send();
+  } catch (e) {
+    return response
+      .status(500)
+      .json({ error: e });
+  }
+}
+
+// DELETE /{oid}
+export const deletePost = async (request: Request, response: Response) => {
+  const { postId } = request.params;
+
+  if (!postId) {
+    return response
+      .status(400)
+      .json({ error: CommonErrors.BadRequest });
+  }
+
+  try {
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return response
+        .status(400)
+        .json({ error: CommonErrors.NotFound });
+    }
+
+    const result = await Post.deleteOne({ _id: post._id });
+
+    return response
+      .status(204)
+      .send();
+  } catch (e) {
+    return response
+      .status(500)
+      .json({ error: e });
+  }
+}
