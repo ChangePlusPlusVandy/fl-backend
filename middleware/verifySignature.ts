7f8ebcd9a3161bd7d44e81d71c1c3be707167723
Gabriel Dong <gabrielhdong@gmail.com>
2024-01-10 13:50:20 -0600

    small formatting:

--- middleware/verifySignature.ts
+++ middleware/verifySignature.ts
@@ -1,5 +1,6 @@
 import { type Request, type Response, type NextFunction } from "express";
 import { createHmac } from "crypto";
+
 export const verifyHmacSignature = async (
   request: Request,
   response: Response,
@@ -8,20 +9,27 @@ export const verifyHmacSignature = async (
   const signatureName = "Friends-Life-Signature";
   const receivedSignature = request?.header(signatureName);
   let requestBody = {};
-  if (Object.keys(request.body).length !== 0) requestBody = request.body;
-  else if (Object.keys(request.params).length !== 0) requestBody = request.params;
-  else if (Object.keys(request.body).length === 0 && Object.keys(request.params).length === 0) requestBody = request.method;
+
+  if (Object.keys(request.body).length !== 0) {
+    requestBody = request.body;
+  } else if (Object.keys(request.params).length !== 0) {
+    requestBody = request.params;
+  } else if (
+    Object.keys(request.body).length === 0 &&
+    Object.keys(request.params).length === 0
+  ) {
+    requestBody = request.method;
+  }
 
   const secretKey = process.env.SECRET_KEY;
 
   if (secretKey === undefined) return false;
 
-  const secret = Buffer.from(secretKey, "base64");
-  const hmac = createHmac("sha256", secret);
-  const calculatedSignature = hmac
-    .update(JSON.stringify(requestBody))
-    .digest()
-    .toString("base64");
+  const calculatedSignature = generateHmacSignature(
+    JSON.stringify(requestBody),
+    secretKey
+  );
+
   if (
     receivedSignature !== undefined &&
     secureCompare(receivedSignature, calculatedSignature)
@@ -30,16 +38,20 @@ export const verifyHmacSignature = async (
   }
   return response.status(401).json({ error: "Unauthorized" });
 };
+
 const secureCompare = (a: string, b: string) => {
   const bufA = Buffer.from(a);
   const bufB = Buffer.from(b);
+
   if (bufA.length !== bufB.length) {
     return false;
   }
+
   let result = 0;
   for (let i = 0; i < bufA.length; i++) {
     result |= bufA[i] ^ bufB[i];
   }
+
   return result === 0;
 };
 
